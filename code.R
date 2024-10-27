649babd5114d14c35cacf7fe1eb2abfdcf96e4d4
Gabriel Caseiro <gabrielcaseiro99@gmail.com>
2024-10-27 18:06:34 -0400

    code_update

--- code.R
+++ code.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(fixest)
 library(haven)
 
-# 1. 
+# 1. Media coverage data ----
 
 dt1<-read_dta('2010_news_data_JOP_dataverse.dta')
   dt1$year<-2010
@@ -18,20 +18,24 @@ dt<-bind_rows(dt1,dt2)
 rm(dt1)
 rm(dt2)
 
+# 2. CCES data ----
+
 cces<-read_dta('CCES_Panel_Full3waves_VV_V4.dta')
 
 cols<-c('caseid',
         'weight',
-        'cdid112_10','countyfips_10','cdid114_14','countyfips_14',
-        'CC10_315a','CC14_315a',
-        'CC10_390','CC14_390','CC14_390b'
+        'cdid112_10','countyfips_10','cdid114_14','countyfips_14', #Districts IDS
+        'CC10_315a','CC14_315a', # Incumbent evaluation
+        'CC10_390','CC14_390','CC14_390b' # Vote intention
         )
 
 cces<-cces %>% 
   select(any_of(cols)) %>% 
-  mutate(`CC14_390`=ifelse(is.na(`CC14_390`),`CC14_390b`,`CC14_390`),
+  mutate(`CC14_390`=ifelse(is.na(`CC14_390`),`CC14_390b`,`CC14_390`), # Adjust variable
          `cdid114_14`=str_pad(`cdid114_14`,2,'left',0)) %>% 
   select(-`CC14_390b`)
+
+# Consolidate panel:
 
 cces<-cces %>% 
   mutate(across(3:10,as.character)) %>% 
@@ -44,14 +48,17 @@ cces<-cces %>%
 cces<-cces %>% 
   pivot_wider(names_from = name,values_from = value)
 
-
 cces<-cces %>% 
-  mutate(ccesdistrict=paste0(str_sub(countyfips,1,2),cdid)) %>% 
+  mutate(ccesdistrict=paste0(str_sub(countyfips,1,2),cdid)) %>% # District id
   mutate(across(4:8,as.numeric))
+
+# Merge with media coverage data:
 
 cces<-cces %>% 
   left_join(dt) %>% 
   filter(!is.na(newspaper))
+
+# CCES variables coding:
 
 cces<-cces %>% 
   mutate(vote_intention=1*(as.numeric(`390`)<=7),
@@ -61,6 +68,7 @@ cces<-cces %>%
   mutate(nomove=1*(length(unique(ccesdistrict))==1)) %>% 
   ungroup()
 
+# First-Diffrence dataset:
 
 cces_fd<-cces %>% 
   arrange(year) %>% 
@@ -70,6 +78,9 @@ cces_fd<-cces %>%
          storycount=storycount-lag(storycount)) %>% 
   ungroup() %>% 
   filter(year==2014)
+
+
+# 3. Analysis ----
 
 reg1fd<-feols(rate_incumbent ~ storycount  ,
             cluster='ccesdistrict',
@@ -96,6 +107,7 @@ reg2<-feols(vote_intention ~ storycount | caseid + year ,
 
 summary(reg2)
 
+# Main table:
 
 etable(reg1fd,reg1,reg2fd,reg2,drop = 'Constant', tex = TRUE)
 
